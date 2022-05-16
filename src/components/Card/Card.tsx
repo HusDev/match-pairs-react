080104bbbafc6f35ff3db64c074d0488fb141292
Hussein Hasan <hussein.hasan@leaguesofcode.com>
2022-05-16 21:43:36 +0200

    convert next to react 🚨

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,46 @@
+import { CardStyle, CardWrapper } from "./Card.styled";
+import { useSpring } from "react-spring";
+import { GameCard } from "../../gameMachine";
+import Logo from "./../../assets/img/logo.png";
+
+interface CardProps {
+  emoji: GameCard;
+  selectCard: (card: GameCard) => void;
+}
+
+const Card = ({ emoji, selectCard }: CardProps) => {
+  const { transform, opacity } = useSpring({
+    opacity: emoji.flip ? 1 : 0,
+    transform: `perspective(600px) rotateY(${emoji.flip ? 180 : 0}deg)`,
+    config: { mass: 5, tension: 500, friction: 80 },
+  });
+
+  return (
+    <CardWrapper
+      onClick={() => {
+        if (!emoji.collected) {
+          selectCard(emoji);
+        }
+      }}
+    >
+      <CardStyle
+        face={"front"}
+        style={{ opacity: opacity.to((o) => 1 - o), transform }}
+      >
+        <img src={Logo} width="100%" alt={"emoji"} />
+      </CardStyle>
+      <CardStyle
+        face={"back"}
+        style={{
+          opacity,
+          transform,
+          rotateY: "180deg",
+        }}
+      >
+        {emoji.content}
+      </CardStyle>
+    </CardWrapper>
+  );
+};
+
+export { Card };
